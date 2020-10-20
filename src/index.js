d2af5bdb76ba4df529977a97f26e5120a2e38a1d
Đăng Nguyễn <nguyenhuedang@gmail.com>
2020-10-20 15:31:58 +0700

    Remove node

--- src/index.js
+++ src/index.js
@@ -61,9 +61,9 @@ export default class TronWeb extends EventEmitter {
         this.plugin = new Plugin(this, options);
         this.utils = utils;
 
-        // this.setFullNode(fullNode);
-        // this.setSolidityNode(solidityNode);
-        // this.setEventServer(eventServer);
+        this.setFullNode(fullNode);
+        this.setSolidityNode(solidityNode);
+        this.setEventServer(eventServer);
 
         this.providers = providers;
         this.BigNumber = BigNumber;
@@ -159,251 +159,251 @@ export default class TronWeb extends EventEmitter {
         return Object.values(providers).some(knownProvider => provider instanceof knownProvider);
     }
 
-    // setFullNode(fullNode) {
-    //     if (utils.isString(fullNode))
-    //         fullNode = new providers.HttpProvider(fullNode);
+    setFullNode(fullNode) {
+        if (utils.isString(fullNode))
+            fullNode = new providers.HttpProvider(fullNode);
 
-    if(!this.isValidProvider(fullNode))
-throw new Error('Invalid full node provided');
+        if (!this.isValidProvider(fullNode))
+            throw new Error('Invalid full node provided');
 
-this.fullNode = fullNode;
-this.fullNode.setStatusPage('wallet/getnowblock');
+        this.fullNode = fullNode;
+        this.fullNode.setStatusPage('wallet/getnowblock');
 
-this.getFullnodeVersion();
+        this.getFullnodeVersion();
     }
 
-setSolidityNode(solidityNode) {
-    if (utils.isString(solidityNode))
-        solidityNode = new providers.HttpProvider(solidityNode);
+    setSolidityNode(solidityNode) {
+        if (utils.isString(solidityNode))
+            solidityNode = new providers.HttpProvider(solidityNode);
 
-    if (!this.isValidProvider(solidityNode))
-        throw new Error('Invalid solidity node provided');
+        if (!this.isValidProvider(solidityNode))
+            throw new Error('Invalid solidity node provided');
 
-    this.solidityNode = solidityNode;
-    this.solidityNode.setStatusPage('walletsolidity/getnowblock');
-}
+        this.solidityNode = solidityNode;
+        this.solidityNode.setStatusPage('walletsolidity/getnowblock');
+    }
 
-setEventServer(...params) {
-    this.event.setServer(...params)
-}
+    setEventServer(...params) {
+        this.event.setServer(...params)
+    }
 
-currentProviders() {
-    return {
-        fullNode: this.fullNode,
-        solidityNode: this.solidityNode,
-        eventServer: this.eventServer
-    };
-}
+    currentProviders() {
+        return {
+            fullNode: this.fullNode,
+            solidityNode: this.solidityNode,
+            eventServer: this.eventServer
+        };
+    }
 
-currentProvider() {
-    return this.currentProviders();
-}
+    currentProvider() {
+        return this.currentProviders();
+    }
 
 
-getEventResult(...params) {
+    getEventResult(...params) {
 
-    if (typeof params[1] !== 'object') {
-        params[1] = {
-            sinceTimestamp: params[1] || 0,
-            eventName: params[2] || false,
-            blockNumber: params[3] || false,
-            size: params[4] || 20,
-            page: params[5] || 1
-        }
-        params.splice(2, 4)
+        if (typeof params[1] !== 'object') {
+            params[1] = {
+                sinceTimestamp: params[1] || 0,
+                eventName: params[2] || false,
+                blockNumber: params[3] || false,
+                size: params[4] || 20,
+                page: params[5] || 1
+            }
+            params.splice(2, 4)
 
-        // callback:
-        if (!utils.isFunction(params[2])) {
+            // callback:
+            if (!utils.isFunction(params[2])) {
 
-            if (utils.isFunction(params[1].page)) {
-                params[2] = params[1].page;
-                params[1].page = 1;
-            } else if (utils.isFunction(params[1].size)) {
-                params[2] = params[1].size;
-                params[1].size = 20;
-                params[1].page = 1;
+                if (utils.isFunction(params[1].page)) {
+                    params[2] = params[1].page;
+                    params[1].page = 1;
+                } else if (utils.isFunction(params[1].size)) {
+                    params[2] = params[1].size;
+                    params[1].size = 20;
+                    params[1].page = 1;
+                }
             }
         }
+
+        return this.event.getEventsByContractAddress(...params);
     }
 
-    return this.event.getEventsByContractAddress(...params);
-}
+    getEventByTransactionID(...params) {
+        return this.event.getEventsByTransactionID(...params)
+    }
 
-getEventByTransactionID(...params) {
-    return this.event.getEventsByTransactionID(...params)
-}
-
-contract(abi = [], address = false) {
-    return new Contract(this, abi, address);
-}
+    contract(abi = [], address = false) {
+        return new Contract(this, abi, address);
+    }
 
     static get address() {
-    return {
-        fromHex(address) {
-            if (!utils.isHex(address))
-                return address;
+        return {
+            fromHex(address) {
+                if (!utils.isHex(address))
+                    return address;
 
-            return utils.crypto.getBase58CheckAddress(
-                utils.code.hexStr2byteArray(address.replace(/^0x/, ADDRESS_PREFIX))
-            );
-        },
-        toHex(address) {
-            if (utils.isHex(address))
-                return address.toLowerCase().replace(/^0x/, ADDRESS_PREFIX);
+                return utils.crypto.getBase58CheckAddress(
+                    utils.code.hexStr2byteArray(address.replace(/^0x/, ADDRESS_PREFIX))
+                );
+            },
+            toHex(address) {
+                if (utils.isHex(address))
+                    return address.toLowerCase().replace(/^0x/, ADDRESS_PREFIX);
 
-            return utils.code.byteArray2hexStr(
-                utils.crypto.decodeBase58Address(address)
-            ).toLowerCase();
-        },
-        fromPrivateKey(privateKey) {
-            try {
-                return utils.crypto.pkToAddress(privateKey);
-            } catch {
-                return false;
+                return utils.code.byteArray2hexStr(
+                    utils.crypto.decodeBase58Address(address)
+                ).toLowerCase();
+            },
+            fromPrivateKey(privateKey) {
+                try {
+                    return utils.crypto.pkToAddress(privateKey);
+                } catch {
+                    return false;
+                }
             }
         }
     }
-}
 
     static sha3(string, prefix = true) {
-    return (prefix ? '0x' : '') + keccak256(Buffer.from(string, 'utf-8')).toString().substring(2);
-}
+        return (prefix ? '0x' : '') + keccak256(Buffer.from(string, 'utf-8')).toString().substring(2);
+    }
 
     static toHex(val) {
-    if (utils.isBoolean(val))
-        return TronWeb.fromDecimal(+val);
+        if (utils.isBoolean(val))
+            return TronWeb.fromDecimal(+val);
 
-    if (utils.isBigNumber(val))
-        return TronWeb.fromDecimal(val);
+        if (utils.isBigNumber(val))
+            return TronWeb.fromDecimal(val);
 
-    if (typeof val === 'object')
-        return TronWeb.fromUtf8(JSON.stringify(val));
+        if (typeof val === 'object')
+            return TronWeb.fromUtf8(JSON.stringify(val));
 
-    if (utils.isString(val)) {
-        if (/^(-|)0x/.test(val))
-            return val;
+        if (utils.isString(val)) {
+            if (/^(-|)0x/.test(val))
+                return val;
 
-        if ((!isFinite(val)) || /^\s*$/.test(val))
-            return TronWeb.fromUtf8(val);
+            if ((!isFinite(val)) || /^\s*$/.test(val))
+                return TronWeb.fromUtf8(val);
+        }
+
+        let result = TronWeb.fromDecimal(val);
+        if (result === '0xNaN') {
+            throw new Error('The passed value is not convertible to a hex string');
+        } else {
+            return result;
+        }
     }
-
-    let result = TronWeb.fromDecimal(val);
-    if (result === '0xNaN') {
-        throw new Error('The passed value is not convertible to a hex string');
-    } else {
-        return result;
-    }
-}
 
     static toUtf8(hex) {
-    if (utils.isHex(hex)) {
-        hex = hex.replace(/^0x/, '');
-        return Buffer.from(hex, 'hex').toString('utf8');
-    } else {
-        throw new Error('The passed value is not a valid hex string');
+        if (utils.isHex(hex)) {
+            hex = hex.replace(/^0x/, '');
+            return Buffer.from(hex, 'hex').toString('utf8');
+        } else {
+            throw new Error('The passed value is not a valid hex string');
+        }
     }
-}
 
     static fromUtf8(string) {
-    if (!utils.isString(string)) {
-        throw new Error('The passed value is not a valid utf-8 string')
+        if (!utils.isString(string)) {
+            throw new Error('The passed value is not a valid utf-8 string')
+        }
+        return '0x' + Buffer.from(string, 'utf8').toString('hex');
     }
-    return '0x' + Buffer.from(string, 'utf8').toString('hex');
-}
 
     static toAscii(hex) {
-    if (utils.isHex(hex)) {
-        let str = "";
-        let i = 0, l = hex.length;
-        if (hex.substring(0, 2) === '0x') {
-            i = 2;
+        if (utils.isHex(hex)) {
+            let str = "";
+            let i = 0, l = hex.length;
+            if (hex.substring(0, 2) === '0x') {
+                i = 2;
+            }
+            for (; i < l; i += 2) {
+                let code = parseInt(hex.substr(i, 2), 16);
+                str += String.fromCharCode(code);
+            }
+            return str;
+        } else {
+            throw new Error('The passed value is not a valid hex string');
         }
-        for (; i < l; i += 2) {
-            let code = parseInt(hex.substr(i, 2), 16);
-            str += String.fromCharCode(code);
-        }
-        return str;
-    } else {
-        throw new Error('The passed value is not a valid hex string');
     }
-}
 
     static fromAscii(string, padding) {
-    if (!utils.isString(string)) {
-        throw new Error('The passed value is not a valid utf-8 string')
+        if (!utils.isString(string)) {
+            throw new Error('The passed value is not a valid utf-8 string')
+        }
+        return '0x' + Buffer.from(string, 'ascii').toString('hex').padEnd(padding, '0');
     }
-    return '0x' + Buffer.from(string, 'ascii').toString('hex').padEnd(padding, '0');
-}
 
 
     static toDecimal(value) {
-    return TronWeb.toBigNumber(value).toNumber();
-}
+        return TronWeb.toBigNumber(value).toNumber();
+    }
 
     static fromDecimal(value) {
-    const number = TronWeb.toBigNumber(value);
-    const result = number.toString(16);
+        const number = TronWeb.toBigNumber(value);
+        const result = number.toString(16);
 
-    return number.isLessThan(0) ? '-0x' + result.substr(1) : '0x' + result;
-}
+        return number.isLessThan(0) ? '-0x' + result.substr(1) : '0x' + result;
+    }
 
     static fromSun(sun) {
-    const trx = TronWeb.toBigNumber(sun).div(1_000_000);
-    return utils.isBigNumber(sun) ? trx : trx.toString(10);
-}
+        const trx = TronWeb.toBigNumber(sun).div(1_000_000);
+        return utils.isBigNumber(sun) ? trx : trx.toString(10);
+    }
 
     static toSun(trx) {
-    const sun = TronWeb.toBigNumber(trx).times(1_000_000);
-    return utils.isBigNumber(trx) ? sun : sun.toString(10);
-}
+        const sun = TronWeb.toBigNumber(trx).times(1_000_000);
+        return utils.isBigNumber(trx) ? sun : sun.toString(10);
+    }
 
     static toBigNumber(amount = 0) {
-    if (utils.isBigNumber(amount))
-        return amount;
+        if (utils.isBigNumber(amount))
+            return amount;
 
-    if (utils.isString(amount) && /^(-|)0x/.test(amount))
-        return new BigNumber(amount.replace('0x', ''), 16);
+        if (utils.isString(amount) && /^(-|)0x/.test(amount))
+            return new BigNumber(amount.replace('0x', ''), 16);
 
-    return new BigNumber(amount.toString(10), 10);
-}
+        return new BigNumber(amount.toString(10), 10);
+    }
 
     static isAddress(address = false) {
-    if (!utils.isString(address))
-        return false;
+        if (!utils.isString(address))
+            return false;
 
-    // Convert HEX to Base58
-    if (address.length === 42) {
+        // Convert HEX to Base58
+        if (address.length === 42) {
+            try {
+                return TronWeb.isAddress(
+                    utils.crypto.getBase58CheckAddress(
+                        utils.code.hexStr2byteArray(address) // it throws an error if the address starts with 0x
+                    )
+                );
+            } catch (err) {
+                return false;
+            }
+        }
         try {
-            return TronWeb.isAddress(
-                utils.crypto.getBase58CheckAddress(
-                    utils.code.hexStr2byteArray(address) // it throws an error if the address starts with 0x
-                )
-            );
+            return utils.crypto.isAddressValid(address);
         } catch (err) {
             return false;
         }
     }
-    try {
-        return utils.crypto.isAddressValid(address);
-    } catch (err) {
-        return false;
-    }
-}
 
     static async createAccount() {
-    const account = utils.accounts.generateAccount();
+        const account = utils.accounts.generateAccount();
 
-    return account;
-}
+        return account;
+    }
 
-async isConnected(callback = false) {
-    if (!callback)
-        return this.injectPromise(this.isConnected);
+    async isConnected(callback = false) {
+        if (!callback)
+            return this.injectPromise(this.isConnected);
 
-    return callback(null, {
-        fullNode: await this.fullNode.isConnected(),
-        solidityNode: await this.solidityNode.isConnected(),
-        eventServer: this.eventServer && await this.eventServer.isConnected()
-    });
-}
+        return callback(null, {
+            fullNode: await this.fullNode.isConnected(),
+            solidityNode: await this.solidityNode.isConnected(),
+            eventServer: this.eventServer && await this.eventServer.isConnected()
+        });
+    }
 };
